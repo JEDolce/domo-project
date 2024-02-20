65674189d2d22abb1da8a750a1c982a9a7679f12
juandolce <juandolce20@gmail.com>
2024-02-20 12:19:30 -0300

    general fixes

--- main/app.js
+++ main/app.js
@@ -1,13 +1,17 @@
 // MOBILE ENVELOPE
 const envelopeMobile = document.querySelector(".envelope");
-const mobileLineUp = document.getElementById("line-up");
-const mobileLineDown = document.getElementById("line-down");
+const envelopeC = document.querySelector(".envelope-c");
+const envelopeD = document.querySelector(".envelope-d");
+const mobileLineUp = document.querySelectorAll("#line-up");
+const mobileLineDown = document.querySelectorAll("#line-down");
 
 // Variable de ancho
 let envelopeMobileWidth = envelopeMobile.offsetWidth;
 
 // Envelope height equal to width for mobile
 envelopeMobile.style.height = `${envelopeMobileWidth}px`;
+envelopeC.style.height = `${envelopeMobileWidth}px`;
+envelopeD.style.height = `${envelopeMobileWidth}px`;
 
 // Variable de altura
 let envelopeMobileHeight = envelopeMobile.offsetHeight;
@@ -17,33 +21,50 @@ let envelopeAngle = Math.atan(envelopeMobileHeight / envelopeMobileWidth) * (180
 let envelopeDiag = envelopeMobileWidth / Math.cos(envelopeAngle * Math.PI / 180);
 
 // Sumo 20px para que lo exceda un poco
-mobileLineUp.style.width = `${envelopeDiag + 20}px`;
-mobileLineUp.style.transform = `rotate(-${envelopeAngle}deg)`;
+mobileLineUp.forEach(i => {
+    i.style.width = `${envelopeDiag + 20}px`;
+    i.style.transform = `rotate(-${envelopeAngle}deg)`;
+});
 
-mobileLineDown.style.width = `${envelopeDiag}px`;
-mobileLineDown.style.transform = `rotate(${envelopeAngle}deg)`;
-
+mobileLineDown.forEach(j => {
+    j.style.width = `${envelopeDiag}px`;
+    j.style.transform = `rotate(${envelopeAngle}deg)`;
+});
 
 
 // DESKTOP ENVELOPE
 const envelopeLg = document.querySelector(".right--envelope");
-const lgLineUp = document.querySelector(".right--envelope--line-up");
-const lgLineDown = document.querySelector(".right--envelope--line-down");
+const envelopeLgC = document.querySelector(".right-c");
+const envelopeLgD = document.querySelector(".right-d");
+const lgLineUp = document.querySelectorAll(".right--envelope--line-up");
+const lgLineDown = document.querySelectorAll(".right--envelope--line-down");
 
 let envelopeLgWidth = envelopeLg.offsetWidth;
 
 envelopeLg.style.height = `${envelopeLgWidth / 1.5}px`;
+envelopeLgC.style.height = `${envelopeLgWidth / 1.5}px`;
+envelopeLgD.style.height = `${envelopeLgWidth / 1.5}px`;
 
 let envelopeLgHeight = envelopeLg.offsetHeight;
 
 let envelopeAngleLg = Math.atan(envelopeLgHeight / envelopeLgWidth) * (180 / Math.PI);
 let envelopeDiagLg = envelopeLgWidth / Math.cos(envelopeAngleLg * Math.PI / 180);
 
-lgLineUp.style.width = `${envelopeDiagLg + 20}px`;
-lgLineUp.style.transform = `rotate(-${envelopeAngleLg}deg)`;
+lgLineUp.forEach(k => {
+    k.style.width = `${envelopeDiagLg + 20}px`;
+    k.style.transform = `rotate(-${envelopeAngleLg}deg)`;
+});
 
-lgLineDown.style.width = `${envelopeDiagLg + 20}px`;
-lgLineDown.style.transform = `rotate(${envelopeAngleLg}deg)`;
+lgLineDown.forEach(l => {
+    l.style.width = `${envelopeDiagLg + 20}px`;
+    l.style.transform = `rotate(${envelopeAngleLg}deg)`;
+});
+
+// lgLineUp.style.width = `${envelopeDiagLg + 20}px`;
+// lgLineUp.style.transform = `rotate(-${envelopeAngleLg}deg)`;
+
+// lgLineDown.style.width = `${envelopeDiagLg + 20}px`;
+// lgLineDown.style.transform = `rotate(${envelopeAngleLg}deg)`;
 
 
 // MAIN SCREEN CROSS
